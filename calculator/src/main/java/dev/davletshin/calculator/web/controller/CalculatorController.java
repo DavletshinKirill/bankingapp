b6b5f1c359de2d146a0bfc431044822ee04497c8
kirill <kirill.davletshin.02@yandex.ru>
2024-11-12 13:30:02 +0300

    Initial Commit

---
+++ calculator/src/main/java/dev/davletshin/calculator/web/controller/CalculatorController.java
@@ -0,0 +1,19 @@
+package dev.davletshin.calculator.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/calculate")
+@RequiredArgsConstructor
+public class CalculatorController {
+
+    @PostMapping("/offers")
+    public void getOffers(){}
+
+    @PostMapping("/calc")
+    public void calculate() {}
+
+}
